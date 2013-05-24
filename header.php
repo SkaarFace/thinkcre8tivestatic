da4c44c86e8e89d3ecdf8a6d1c892d9ade768e9f
Sean Skaar <sean.skaar@gmail.com>
2013-05-24 15:10:43 -0500

    For the weekend

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="./stylesheets/header.css">
+        <link rel="screen.css" href="./stylesheets/screen.css">
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" language="javascript" charset="utf-8" src="js/menu.js"></script>  <script type="text/javascript" language="javascript" charset="utf-8" src="menu.js"></script>
+</head>
+<body>
+<header id = "topnav">
+	<h1>Think Cre8tive</h1>
+	<nav>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Some Link</a></li>
+		</ul>
+	</nav>
+	<a href = "#" id = "navbtn">Nav Menu</a>
+</header>
